d381df73a52f6f15311f8818d908ced5c3621528
Marcos Tanaka <marcosatanaka@gmail.com>
2020-06-24 00:24:48 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# sidebar-ios14
-Usage example of the new iOS 14 sidebar UI element
+# Sidebar iOS 14
+
+This is a sample project demonstrating how to implement the new Sidebar UI on an iPad app using Swift. It tries to mimic the UI of the new Music app on iPadOS 14.0:
+
+![Preview.png](Preview.png)
+
+It contains just the code required to create the UI, and it was based on the Apple's sample project  [Implementing Modern Collection Views](https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/implementing_modern_collection_views).
+
+## TO-DO
+
+- [ ] Customize the accent color.
+- [ ] Maintain selection when contracting/expanding a section.
+- [ ] Revert to tab bar based navigation on compact layouts, and on the iPhone.
+
+## Documentation links
+
+- [Human Interface Guidelines for the Sidebar](https://developer.apple.com/design/human-interface-guidelines/ios/bars/sidebars/)
+- [Updated UISplitViewController documentation](https://developer.apple.com/documentation/uikit/uisplitviewcontroller)
+
+## WWDC20 videos
+
+- [Design for iPad](https://developer.apple.com/videos/play/wwdc2020/10206/)
+- [Advances in UICollectionView](https://developer.apple.com/videos/play/wwdc2020/10097/)
+- [Advances in diffable data sources](https://developer.apple.com/videos/play/wwdc2020/10045/)
+- [Lists in UICollectionView](https://developer.apple.com/videos/play/wwdc2020/10026/)
+- [Modern cell configuration](https://developer.apple.com/videos/play/wwdc2020/10027/)
---
+++ Sidebar/SidebarViewController.swift
@@ -0,0 +1,139 @@
+import UIKit
+
+class SidebarViewController: UIViewController {
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>! = nil
+    private var collectionView: UICollectionView! = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Music"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        configureHierarchy()
+        configureDataSource()
+        addNavigationButtons()
+    }
+
+    private func addNavigationButtons() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: nil)
+    }
+}
+
+// MARK: - Layout
+
+extension SidebarViewController {
+
+    private func createLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { section, layoutEnvironment in
+            var config = UICollectionLayoutListConfiguration(appearance: .sidebar)
+            config.headerMode = section == 0 ? .none : .firstItemInSection
+            return NSCollectionLayoutSection.list(using: config, layoutEnvironment: layoutEnvironment)
+        }
+    }
+
+}
+
+// MARK: - Data
+
+extension SidebarViewController {
+
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+    }
+    
+    private func configureDataSource() {
+        // Configuring cells
+
+        let headerRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { (cell, indexPath, item) in
+            var content = cell.defaultContentConfiguration()
+            content.text = item.title
+            cell.contentConfiguration = content
+            cell.accessories = [.outlineDisclosure()]
+        }
+        
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { (cell, indexPath, item) in
+            var content = cell.defaultContentConfiguration()
+            content.text = item.title
+            content.image = item.image
+            cell.contentConfiguration = content
+            cell.accessories = []
+        }
+
+        // Creating the datasource
+
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: Item) -> UICollectionViewCell? in
+            if indexPath.item == 0 && indexPath.section != 0 {
+                return collectionView.dequeueConfiguredReusableCell(using: headerRegistration, for: indexPath, item: item)
+            } else {
+                return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+            }
+        }
+
+        // Creating and applying snapshots
+
+        let sections: [Section] = [.tabs, .library, .playlists]
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections(sections)
+        dataSource.apply(snapshot, animatingDifferences: false)
+
+        for section in sections {
+            switch section {
+            case .tabs:
+                var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<Item>()
+                sectionSnapshot.append(tabsItems)
+                dataSource.apply(sectionSnapshot, to: section)
+            case .library:
+                let headerItem = Item(title: section.rawValue, image: nil)
+                var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<Item>()
+                sectionSnapshot.append([headerItem])
+                sectionSnapshot.append(libraryItems, to: headerItem)
+                sectionSnapshot.expand([headerItem])
+                dataSource.apply(sectionSnapshot, to: section)
+            case .playlists:
+                let headerItem = Item(title: section.rawValue, image: nil)
+                var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<Item>()
+                sectionSnapshot.append([headerItem])
+                sectionSnapshot.append(playlistItems, to: headerItem)
+                sectionSnapshot.expand([headerItem])
+                dataSource.apply(sectionSnapshot, to: section)
+            }
+        }
+    }
+
+}
+
+// MARK: - Structs and sample data
+
+struct Item: Hashable {
+    let title: String?
+    let image: UIImage?
+    private let identifier = UUID()
+}
+
+let tabsItems = [Item(title: "Listen Now", image: UIImage(systemName: "play.circle")),
+                 Item(title: "Browse", image: UIImage(systemName: "square.grid.2x2")),
+                 Item(title: "Radio", image: UIImage(systemName: "dot.radiowaves.left.and.right")),
+                 Item(title: "Search", image: UIImage(systemName: "magnifyingglass"))]
+
+let libraryItems = [Item(title: "Recently Added", image: UIImage(systemName: "clock")),
+                    Item(title: "Artists", image: UIImage(systemName: "music.mic")),
+                    Item(title: "Albums", image: UIImage(systemName: "rectangle.stack")),
+                    Item(title: "Songs", image: UIImage(systemName: "music.note")),
+                    Item(title: "Music Videos", image: UIImage(systemName: "tv.music.note")),
+                    Item(title: "TV & Movies", image: UIImage(systemName: "tv"))]
+
+let playlistItems = [Item(title: "All Playlists", image: UIImage(systemName: "music.note.list")),
+                     Item(title: "Replay 2015", image: UIImage(systemName: "music.note.list")),
+                     Item(title: "Replay 2016", image: UIImage(systemName: "music.note.list")),
+                     Item(title: "Replay 2017", image: UIImage(systemName: "music.note.list")),
+                     Item(title: "Replay 2018", image: UIImage(systemName: "music.note.list")),
+                     Item(title: "Replay 2019", image: UIImage(systemName: "music.note.list")),]
+
+enum Section: String {
+    case tabs
+    case library = "Library"
+    case playlists = "Playlists"
+}
